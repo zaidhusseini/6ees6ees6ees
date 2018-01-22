5e3fb8df71069780edabc77d1cd66b5c1b70d344
zaid <zaidhusseini@gmail.com>
2018-01-22 13:31:01 -0800

    Implemenet ES6 BeesBeesBees Subclassing Patterns

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
   // TODO..
+  constructor(){
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(){
+    this.treasureChest.push('treasure');
+  }
+
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,12 @@
 class Grub {
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  };
+
+  eat(){
+
+    return 'Mmmmmmmmm jelly';
+  };
 };
